148f4362b18ce1af46f56c1cf6bd0c1031a82c67
Akshay Gollapudi <123523480+Akshay201021@users.noreply.github.com>
2025-07-03 12:46:37 -0500

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const lightbox = GLightbox({
+      selector: '.glightbox',
+      keyboardNavigation: true,
+      touchNavigation: true,
+      loop: true
+    });
+  });
+  
